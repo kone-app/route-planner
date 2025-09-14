11ea28e4ec3dba69f8b848f3fc405c8dcc0f8d7a
Sushil Kumar <kaairatechsoft@gmail.com>
2025-09-14 22:51:38 +0300

    commeted removed

--- src/journey_service/digitransit.py
+++ src/journey_service/digitransit.py
@@ -62,7 +62,6 @@ def query_journeys(origin_coordinates, destination_coordinates, arriveBy):
     }"""
 
 
-    #routing_url = "https://api.digitransit.fi/routing/v2/hsl/gtfs/v1"
     response = requests.post(
         url=ROUTING_URL,
         headers={"Content-Type": "application/json", "digitransit-subscription-key": API_KEY},
